de7a7ddedc450a3f3d1441f0cbbb5fc2c8a6522f
woyteck <w.hoffmann@b4b.net.pl>
2024-11-06 16:28:43 +0100

    finished xyz task

--- cmd/xyz/main.go
+++ cmd/xyz/main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
+	"net/http"
+	"net/url"
+	"strings"
 
 	"github.com/joho/godotenv"
 	"github.com/mendableai/firecrawl-go"
+	"golang.org/x/net/html"
 	"woyteck.pl/ai_devs3/internal/di"
 	"woyteck.pl/ai_devs3/internal/openai"
 )
@@ -18,13 +23,15 @@ func main() {
 
 	container := di.NewContainer(di.Services)
 
-	pageContents := scrapePage(container)
-	fmt.Println(pageContents)
-	answer := answerQuestion(container, pageContents)
+	question := getQuestion(container)
+	fmt.Println(question)
+	answer := answerQuestion(container, question)
 	fmt.Println(answer)
+
+	postVariables("https://xyz.ag3nts.org/", "tester", "574e112a", answer)
 }
 
-func scrapePage(container *di.Container) string {
+func getQuestion(container *di.Container) string {
 	scraper, ok := container.Get("scraper").(*firecrawl.FirecrawlApp)
 	if !ok {
 		panic("scraper factory failed")
@@ -32,13 +39,36 @@ func scrapePage(container *di.Container) string {
 
 	crawlParams := &firecrawl.ScrapeParams{
 		IncludeTags: []string{"p"},
+		Formats:     []string{"html", "markdown"},
 	}
 	results, err := scraper.ScrapeURL("https://xyz.ag3nts.org/", crawlParams)
 	if err != nil {
 		panic(err)
 	}
 
-	return results.Markdown
+	fmt.Println(results.HTML)
+	doc, err := html.Parse(strings.NewReader(results.HTML))
+	if err != nil {
+		panic(err)
+	}
+
+	return findHumanQuestion(doc)
+}
+
+func findHumanQuestion(n *html.Node) string {
+	if n.Type == html.ElementNode && n.Data == "p" {
+		for _, attr := range n.Attr {
+			if attr.Key == "id" && attr.Val == "human-question" {
+				return n.LastChild.Data
+			}
+		}
+	}
+	for c := n.LastChild; c != nil; c = c.NextSibling {
+		if result := findHumanQuestion(c); result != "" {
+			return result
+		}
+	}
+	return ""
 }
 
 func answerQuestion(container *di.Container, question string) string {
@@ -50,7 +80,7 @@ func answerQuestion(container *di.Container, question string) string {
 	messages := []openai.Message{
 		{
 			Role:    "system",
-			Content: "I search for a question in given text and answer it. I disregard all other text. I return only the answer to the question, nothing else. My answers are concise.",
+			Content: "I search for a question in given text and answer it. I disregard all other text. I return only the answer to the question, nothing else. My answers are always an integer",
 		},
 		{
 			Role:    "user",
@@ -59,4 +89,30 @@ func answerQuestion(container *di.Container, question string) string {
 	}
 	resp := llm.GetCompletionShort(messages, "gpt-3.5-turbo")
 	return resp.Choices[0].Message.Content
+}
+
+func postVariables(pageUrl, username, password, answer string) {
+	form := url.Values{}
+	form.Add("username", username)
+	form.Add("password", password)
+	form.Add("answer", answer)
+
+	request, err := http.NewRequest("POST", pageUrl, strings.NewReader(form.Encode()))
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	if err != nil {
+		panic(err)
+	}
+
+	var client = &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		panic(err)
+	}
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(body))
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,9 @@ go 1.22.5
 
 toolchain go1.22.8
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/joho/godotenv v1.5.1
+	golang.org/x/net v0.30.0
+)
 
-require github.com/mendableai/firecrawl-go v1.0.0 // indirect
+require github.com/mendableai/firecrawl-go v1.0.0
